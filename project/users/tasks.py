17a3462c045632e9b0bf9377f516ce268d91d978
MatthieuLeNozach <matthieu.le.nozach@gmail.com>
2024-06-12 11:46:58 +0200

    grf

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 
 @shared_task()
 def divide(x, y):
+    from celery.contrib import rdb
+    rdb.set_trace()
     import time
-    time.sleep(5)
+    time.sleep(6)
     return x / y
---
+++ project/users/views.py
@@ -0,0 +1,51 @@
+from loguru import logger
+import random
+
+import requests
+from celery.result import AsyncResult
+from fastapi import FastAPI, Request, Body
+from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+
+from project.users import users_router
+from project.users.schemas import UserBody
+from project.users.tasks import sample_task
+
+
+templates = Jinja2Templates(directory="project/users/templates")
+
+def api_call(email: str):
+    # Testing a failed api call
+    if random.choice([0, 1]):
+        raise Exception("random processing error")
+    
+    # Simulating a call to a third party api
+    requests.post("https://httpbin.org/delay/5")
+    
+
+@users_router.get("/form/")
+def form_example_get(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
+
+
+@users_router.post("/form/")
+def form_example_post(user_body: UserBody):
+    task = sample_task.delay(user_body.email)
+    return JSONResponse({"task_id": task.task_id})
+
+@users_router.get("/task_status/")
+def task_status(task_id: str):
+    task = AsyncResult(task_id)
+    state = task.state
+
+    if state == "FAILURE":
+        error = str(task.result)
+        response = {
+            "state": state,
+            "error": error
+        }
+    else:
+        response = {
+            "state": state,
+        }
+    return JSONResponse(response)
